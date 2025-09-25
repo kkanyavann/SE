c77573904631e588ae3b507a908ffdcc95e1025a
Pemika Chongkwanyuen <paeng.pemika@gmail.com>
2025-09-25 10:43:03 +0700

    Add console.log form Pang

--- a.js
+++ a.js
@@ -1,2 +1,3 @@
-console.log('Hello from a.js');
-console.log('try to use git stash all and commit at once');
+console.log("Hello from a.js");
+console.log("try to use git stash all and commit at once");
+console.log("Hello World from Pang eiei");
