e09c70438ef04c64e17b985298350f035928494d
kkanyavann <kkanyavann@gmail.com>
2025-09-25 09:49:38 +0700

    try stage all file in once commit

--- a.js
+++ a.js
@@ -1,1 +1,2 @@
 console.log('Hello from a.js');
+console.log('try to use git stash all and commit at once');
--- b.js
+++ b.js
@@ -1,1 +1,2 @@
 console.log('Hello from b.js');
+console.log('try to use git add all and commit at once');
